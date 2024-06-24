3c37e6fb0883a09a1606ddd8f6b7ff4f0b0491ce
Yi Chae Hwan <choir.ch26@gmail.com>
2024-06-24 17:16:21 +0900

    커밋이에요

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 ## 함수 선언부
 def add_num(n1, n2):
     return n1 + n2
+
 def sub_num(n1, n2):
     return n1 - n2
-def mul_num(n1, n2):
-    return n1 * n2
-def div_num(n1, n2):
-    return n1 / n2
-def squ_num(n1, n2):
-    return n1 ^ n2
 
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
@@ -19,12 +14,3 @@ print(num1, '+', num2, '=', res)
 
 res = sub_num(num1, num2)
 print(num1, '-', num2, '=', res)
-
-res = mul_num(num1, num2)
-print(num1, '*', num2, '=', res)
-
-res = div_num(num1, num2)
-print(num1, '/', num2, '=', res)
-
-res = squ_num(num1, num2)
-print(num1, '^', num2, '=', res)
