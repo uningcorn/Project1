f087247fd8f902b50ed0282bae2ecb3ff8ad50c2
Yi Chae Hwan <choir.ch26@gmail.com>
2024-06-24 15:29:41 +0900

    두번째 커밋 계산기 만드는중 빼기기능 추가

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 def add_num(n1, n2):
     return n1 + n2
 
+def sub_num(n1, n2):
+    return n1 - n2
 
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
@@ -9,3 +11,6 @@ num1, num2, res = 100, 200, 0
 ## 메인 코드부
 res = add_num(num1, num2)
 print(num1, '+', num2, '=', res)
+
+res = sub_num(num1, num2)
+print(num1, '-', num2, '=', res)
